e8308e9a67f24a55efd7a1376d88f9b1b1752323
Kazuki Asaka <akazuki@Kazukinonotobukkukonpyuta.local>
2024-12-03 19:42:26 +0900

    connect 成功

--- src/wordpress/install.sh
+++ src/wordpress/install.sh
@@ -6,17 +6,23 @@ echo $MYSQL_DATA_BASE 1>&2
 echo $MYSQL_USER 1>&2
 echo $MYSQL_USER_PASSWORD 1>&2
 
-cd /var/www/html && \
+cd /var/www/html/wordpress && \
 wp core config\
     --dbname="${MYSQL_DATA_BASE}" \
     --dbuser="${MYSQL_USER}" \
     --dbpass="${MYSQL_USER_PASSWORD}" \
     --dbhost=mariadb:3306 \
-    --path="/var/www/html" \
+    --path="/var/www/html/wordpress" \
     --allow-root || { echo config create failed 1>&2;  exit 1; }
 
 
-wp core install --path="/var/www/html" \
+wp core install --title=test \
+                --admin_user=wp_admin \
+                --admin_password=1234 \
+                --admin_email=abc@gmail.com \
+                --skip-email \
+                --url=http://localhost \
+                --path="/var/www/html/wordpress" \
                 --allow-root || { echo core install failed 1>&2;  exit 1; }
 
 php-fpm7.4 -F
